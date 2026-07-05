8000929e4ace4d54510b61db4210bebbfc11247b
monixvision <monixvision@gmail.com>
2026-07-05 15:10:53 +0200

    ya la parte del cartel está completada tanto la pegatina decorativa como el hecho de que cuando le des a la X se cierre y salga como un "acceso directo" easter egg

--- src/pages/Articulo.jsx
+++ src/pages/Articulo.jsx
@@ -1,4 +1,5 @@
 import { useParams, /* Navigate, */ Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import { CATALOGO } from '../data/films.js';
 /* import Error404 from './Error404.jsx'; */
@@ -8,6 +9,11 @@ import tiraRollText from '../assets/img/tira-roll-text.svg';
 import styles from './Articulo.module.css';
 import LabelCard from '../components/LabelCard.jsx';
 import CierrePopUp from '../components/CierrePopUp.jsx';
+
+import pegatinaCartel from '../assets/img/pegatinas/pegatina-icon-cartel.svg';
+/* import pegatinaDesplegable from '../assets/img/pegatinas/pegatina-icon-desplegable.svg'; */
+import iconCartel from '../assets/img/icon/articulo-cartel-icon.svg';
+/* import iconDesplegable from '../assets/img/icon/articulo-desplegable-icon.svg'; */
 
 const Articulo = () => {
     /* 1. Capturamos el slug de la URL */
@@ -23,6 +29,9 @@ const Articulo = () => {
 
     /* 4.Si la pelicula existe, mostramos su información */
 
+    /* está el articulo abierto? */
+    const [popupIsOpen, setpopupIsOpen] = useState(true);
+
     return (
         <div className="relative pt-8">
             <div className="absolute bottom-full right-0 -mb-11 z-10 ml-27.5">
@@ -31,7 +40,7 @@ const Articulo = () => {
 
                 {/* He tenido que crear un componente al svg para poder ponerle el hover como className y además hacer el group-hover para que se haga en modo grupo */}
             </div>
-            <div className="pt-7 pb-6 bg-blue rounded-l-4xl rounded-br-4xl">
+            <div className="pt-7 pb-6 bg-blue rounded-tl-4xl">
                 <div className="flex flex-col gap-7 px-6">
                     <div className="relative flex flex-col gap-4 md:flex-row md:gap-8">
                         <Link to="/Entradas" className="absolute z-997 top-0 right-0 w-auto h-auto">
@@ -52,12 +61,23 @@ const Articulo = () => {
                         style={{ backgroundImage: `url(${tiraRollText})` }}
                         className={`w-full h-4 ${styles.roll}`}>
                     </div>
+                    {/* TO DO: quizás plantear un roll que conforme scrollees se lea */}
                 </div>
-                <div className="relative flex flex-col gap-7 px-10">
+                <div className={`relative flex flex-col gap-7 px-10 ${popupIsOpen ? 'block' : 'hidden'}`}>
                     <div className="p-6 bg-white rounded-xl">
-                        <CierrePopUp text="Cartel" setpopupIsOpen={() => { }} className="absolute top-0 right-0 left-0 mx-10" />
+                        <CierrePopUp text="Cartel" setpopupIsOpen={setpopupIsOpen} className="absolute top-0 right-0 left-0 mx-10" />
 
                         <img src={cartel} alt={tituloEsp} className="w-full rounded-2xl mt-13" />
+                        <img src={pegatinaCartel} alt="Pegatina cartel" className="absolute top-40 right-5 w-20 h-20 rotate-6" />
+                    </div>
+                </div>
+                <div className={`mx-auto flex flex-col ${popupIsOpen ? 'hidden' : 'block'}`} onClick={() => setpopupIsOpen(true)}>
+                    <div className="relative w-20 h-20 mx-auto">
+                        <img src={pegatinaCartel} alt="Pegatina cartel" className="mx-auto" />
+                        <img src={iconCartel} alt="Icono cartel" className="absolute bottom-1 -right-1 w-12 h-12" />
+                    </div>
+                    <div className="mx-auto text-center bg-white dark:bg-black text-2xl font-pixel rounded-full px-5 py-1 text-black dark:text-white uppercase">
+                        Cartel
                     </div>
                 </div>
             </div >
