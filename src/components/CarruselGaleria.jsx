a89df309ba7ce9bcee62fef5b9666a1fcbef4548
monixvision <monixvision@gmail.com>
2026-07-08 21:11:25 +0200

    hemos creado el componente carrusel desde el código de la pagina embla añadiendo el loop, que se vea la primera imagen al porcentaje de forma que de a entender que es un carrusel y ya con las iamgenes jugando con el tailwind y css

--- src/components/CarruselGaleria.jsx
+++ src/components/CarruselGaleria.jsx
@@ -1,10 +1,57 @@
+import useEmblaCarousel from 'embla-carousel-react';
+import {
+    NextButton,
+    PrevButton,
+    usePrevNextButtons
+} from './BtnCarrusel';
+import { DotButton, CarruselIndicadores } from './CarruselIndicadores';
 
-const CarruselGaleria = () => {
+const CarruselGaleria = ({ imagenesGaleria }) => {
+    const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'start', loop: true })
+
+    const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi)
+
+    const { selectedIndex, scrollSnaps, onDotButtonClick } =
+        CarruselIndicadores(emblaApi)
+
     return (
-        <div>
-            <div></div>
+        <div className="relative">
+            <div className="overflow-hidden" ref={emblaRef}>
+                <div className="flex touch-pan-y touch-pinch-zoom -ml-6">
+                    {/* esto es para cuando le des con el dedo arrastrar o agrandar */}
+                    {imagenesGaleria.map((imagen, posicion) => (
+                        <div className="shrink-0 grow-0 basis-full min-w-0 pl-6 first:basis-[85%]" key={posicion}>
+                            <div className="w-full h-full overflow-hidden rounded-xl relative bg-olive-300">
+                                <img
+                                    src={imagen}
+                                    alt={`Slide ${posicion + 1}`}
+                                    // El object-cover ahora recortará la foto de forma inteligente adaptándose 
+                                    // al nuevo ancho de la primera diapositiva sin estirarla ni pixelarla.
+                                    className="w-full h-full object-cover object-center will-change-transform transform translate-z-0"
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Optional: Add navigation controls here */}
+            <div className="absolute bottom-0 top-0 left-0 right-0 flex  justify-between mx-3 mb-6">
+                <PrevButton disabled={prevBtnDisabled} onClick={onPrevButtonClick} />
+                <NextButton disabled={nextBtnDisabled} onClick={onNextButtonClick} />
+            </div>
+            <div className="flex justify-center gap-3 mt-6">
+                {scrollSnaps.map((_, index) => (
+                    <DotButton
+                        key={index}
+                        onClick={() => onDotButtonClick(index)}
+                        className="w-4 h-4">
+                        <div className={`w-full h-full rounded-full ${index === selectedIndex ? 'bg-green' : 'bg-black opacity-50'}`}></div>
+                    </DotButton>
+                ))}
+            </div>
         </div>
-    );
+    )
 };
 
 export default CarruselGaleria;
