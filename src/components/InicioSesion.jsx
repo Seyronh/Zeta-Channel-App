8e28be025e9fc538e46ddeb0ffd4e4c62f365f15
monixvision <monixvision@gmail.com>
2026-07-17 00:31:51 +0200

    inicio de sesion y registro de user

--- src/components/InicioSesion.jsx
+++ src/components/InicioSesion.jsx
@@ -7,7 +7,7 @@ import ojoIcon from '../assets/img/icon/icon-ojo.svg';
 import ojoIconCerrado from '../assets/img/icon/icon-cerrado.svg';
 import checkIcon from '../assets/img/icon/check-icon.svg';
 
-const IncioSesion = () => {
+const IncioSesion = ({ setUserIniciado, setUserRegistrado }) => {
 
     const [email, setEmail] = useState('');
     const [esVisible, setEsVisible] = useState(false);
@@ -18,7 +18,8 @@ const IncioSesion = () => {
     }
 
     return (
-        < div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-6">
+            <h2 className="text-xl font-sans font-bold uppercase text-black dark:text-white mb-4">Iniciar Sesión</h2>
             <FormsInput
                 label="Correo Electrónico"
                 placeholder="ejemplo@gmail.com"
@@ -38,35 +39,20 @@ const IncioSesion = () => {
                 icono={esVisible ? ojoIconCerrado : ojoIcon}
                 onClickIcono={() => setEsVisible(!esVisible)}
             />
-            <a href="#" className="text-black -mt-1 font-semibold font-sans hover:underline">¿Olvidaste tu contraseña?</a>
-            <Btn to="#" text="INICIAR SESIÓN" variant='solidgreen' size='xs' font='sans' />
-            <p className="text-sm text-gray-600">
+            <a href="#" className="text-black dark:text-white -mt-3 font-semibold font-sans hover:underline">¿Olvidaste tu contraseña?</a>
+            <Btn to="#" text="INICIAR SESIÓN" variant='solidgreen' size='xs' font='sans' onClick={() => setUserIniciado(true)} />
+            <p className="text-sm text-gray-600 dark:text-gray-300">
                 Al continuar, aceptas las
-                <span className="text-blue-500 hover:underline">Condiciones de uso</span> y la
-                <span className="text-blue-500 hover:underline">Política de Privacidad</span> del festival
+                <span className="text-blue-500 hover:underline"> Condiciones de uso</span> y la
+                <span className="text-blue-500 hover:underline"> Política de Privacidad</span> del festival
             </p>
-            <div>
-                <p>¿No tienes una cuenta?</p>
-                <Btn to="#" text="REGÍSTRATE" variant='solidpink' size='xs' font='sans' />
+            <div className="flex flex-col gap-2">
+                <p className="text-black dark:text-white">
+                    ¿No tienes una cuenta?
+                </p>
+                <Btn to="#" text="REGÍSTRATE" variant='solidpink' size='xs' font='sans' className="w-full" onClick={() => setUserRegistrado(true)} />
             </div>
-            <div className="bg-black h-0.5 w-full mt-8"></div>
-            <div className="my-3">
-                <h2 className="text-2xl uppercase text-balance text-center text-black font-pixel dark:text-white my-4">Es mucho mejor cuando tienes cuenta porque...</h2>
-                <ul className="flex flex-col gap-6 mt-8 text-black dark:text-white font-sans text-pretty">
-                    <li className="flex flex-col gap-2">
-                        <h3 className="font-semibold">Agenda Personal</h3>
-                        <p>Organiza tus reservas y actividades sin preocuparte de nada.</p>
-                    </li>
-                    <li className="flex flex-col gap-2">
-                        <h3 className="font-semibold">Tus Entradas</h3>
-                        <p>Acceso inmediato a tus tickets y reservas. Olvídate de buscar en el email.</p>
-                    </li>
-                    <li className="flex flex-col gap-2">
-                        <h3 className="font-semibold">Descuentos VIP</h3>
-                        <p>Ahorra con ofertas exclusivas solo para usuarios registrados.</p>
-                    </li>
-                </ul>
-            </div>
+
         </div>
     );
 };
