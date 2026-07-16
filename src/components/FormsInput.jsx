ce8a25bc8465a72a601fa6027fafe8b76ac85607
monixvision <monixvision@gmail.com>
2026-07-17 00:31:33 +0200

    cambio para poner la version dark

--- src/components/FormsInput.jsx
+++ src/components/FormsInput.jsx
@@ -3,7 +3,7 @@ const FormsInput = ({ label, placeholder, variant = 'bordered', onClickIcono, ic
     const baseStyles = "w-full bg-white text-black text-lg placeholder:text-gray-500 border-2 rounded-xl px-4 py-2.5 transition-all outline-none border-2 focus:scale-101 font-sans";
 
     const variantStyles = {
-        bordered: "border-black focus:border-blue",
+        bordered: "border-black focus:border-blue dark:border-neutral-200",
         correct: "border-green focus:border-hover-green",
         incorrect: "border-red focus:border-red-500"
     };
@@ -12,7 +12,7 @@ const FormsInput = ({ label, placeholder, variant = 'bordered', onClickIcono, ic
 
     return (
         <div className="flex flex-col gap-2">
-            <label className="text-lg text-black">{label}</label>
+            <label className="text-lg text-black dark:text-white">{label}</label>
             <div className="relative">
                 {icono && (
                     <img
