c12eb0483ced741b9719369d87098bdf565f0f6e
monixvision <monixvision@gmail.com>
2026-07-19 13:16:36 +0200

    home ya adaptada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,74 +100,78 @@ const Home = ({ popupHome, setpopupHome, popupQuepasa, setpopupQuepasa }) => {
                         </div>
                     )}
                     {popupQuepasa && (
-                        <div className={`relative mt-13 pb-10 mx-auto flex flex-col px-4 md:px-10 lg:px-100 ${popupQuepasa ? 'block' : 'hidden'}`}>
-                            <div className="pt-20 px-6 bg-white rounded-xl">
+                        <div className={`relative mt-13 pb-10 mx-auto flex flex-col px-4 md:px-10 lg:px-100 md:pb-30 ${popupQuepasa ? 'block' : 'hidden'}`}>
+                            <div className="pt-20 px-6 bg-white rounded-xl lg:px-12">
                                 <CierrePopUp text="alerta festival" setpopupIsOpen={setpopupQuepasa} className="absolute top-0 right-0 left-0 mx-4 md:mx-10 lg:mx-100 bg-blue" />
 
-                                <div className="flex flex-col gap-8 items-center text-center mb-12 md:mt-12 md:gap-12">
-                                    <h1 className="text-5xl font-bold font-sans md:text-7xl">Estás viendo Zeta Channel</h1>
-                                    <h2 className="text-2xl font-medium md:text-3xl text-balance">Tu infancia merece un reencuentro</h2>
-                                    <Btn to="/Tickets" text="Compra tu entrada" />
-                                </div>
-                                <div className="flex flex-col gap-4 mx-auto mb-12 md:flex-row md:pt-6 ">
+                                <div className="lg:flex lg:gap-12 lg:items-center md:pb-16">
 
-                                    {/* FOTO GRANDE PRINCIPAL*/}
-                                    <div className="w-full aspect-3/4 rounded-xl overflow-hidden bg-slate-200">
-                                        <img
-                                            src={fotos[fotoSeleccionada]}
-                                            alt={`Foto ${fotoSeleccionada + 1}`}
-                                            className="w-full h-full object-cover"
-                                        />
+                                    <div className="flex flex-col gap-8 items-center text-center mb-12 md:mt-12 md:gap-12 lg:gap-8 lg:mt-6 lg:flex-1/2">
+                                        <h1 className="text-5xl font-bold font-sans md:text-7xl lg:text-6xl">Estás viendo Zeta Channel</h1>
+                                        <h2 className="text-2xl font-medium md:text-3xl text-balance">Tu infancia merece un reencuentro</h2>
+                                        <Btn to="/Tickets" text="Compra tu entrada" />
                                     </div>
+                                    <div className="flex flex-col gap-4 mx-auto mb-12 md:flex-row md:pt-6 lg:flex-1/2">
 
-                                    {/* CONTENEDOR DE MINIATURAS (Dividido en Bloque Izquierdo y Bloque Derecho) */}
-                                    <div className="grid grid-cols-3 gap-3 w-full md:w-1/2 lg:w-full md:grid-cols-1">
+                                        {/* FOTO GRANDE PRINCIPAL*/}
+                                        <div className="w-full aspect-3/4 rounded-xl overflow-hidden bg-slate-200 md:rounded-2xl">
+                                            <img
+                                                src={fotos[fotoSeleccionada]}
+                                                alt={`Foto ${fotoSeleccionada + 1}`}
+                                                className="w-full h-full object-cover"
+                                            />
+                                        </div>
 
-                                        {/* BLOQUE IZQUIERDO: Las 6 primeras fotos (2 filas de 3) */}
-                                        <div className="col-span-2 grid grid-cols-3 gap-3 md:grid-cols-2 ">
-                                            {fotos.slice(0, 6).map((foto, index) => (
-                                                <div
-                                                    key={index}
-                                                    onClick={() => setFotoSeleccionada(index)}
-                                                    className={`
+                                        {/* CONTENEDOR DE MINIATURAS (Dividido en Bloque Izquierdo y Bloque Derecho) */}
+                                        <div className="grid grid-cols-3 gap-3 w-full md:w-1/2 md:grid-cols-1 lg:w-2/4">
+
+                                            {/* BLOQUE IZQUIERDO: Las 6 primeras fotos (2 filas de 3) */}
+                                            <div className="col-span-2 grid grid-cols-3 gap-3 md:grid-cols-2 ">
+                                                {fotos.slice(0, 6).map((foto, index) => (
+                                                    <div
+                                                        key={index}
+                                                        onClick={() => setFotoSeleccionada(index)}
+                                                        className={`
                         relative overflow-hidden cursor-pointer rounded-2xl aspect-square transition-all duration-200
                         ${index === fotoSeleccionada ? 'ring-4 ring-green scale-[0.98]' : 'hover:scale-[1.02]'}
                     `}
-                                                >
-                                                    <img
-                                                        src={foto}
-                                                        alt={`Foto ${index + 1}`}
-                                                        className="w-full h-full object-cover"
-                                                    />
-                                                </div>
-                                            ))}
-                                        </div>
+                                                    >
+                                                        <img
+                                                            src={foto}
+                                                            alt={`Foto ${index + 1}`}
+                                                            className="w-full h-full object-cover"
+                                                        />
+                                                    </div>
+                                                ))}
+                                            </div>
 
-                                        {/* BLOQUE DERECHO: La foto del index 6 (Ocupa las 2 columnas restantes y toda la altura) */}
-                                        {fotos[6] && (
-                                            <div
-                                                onClick={() => setFotoSeleccionada(6)}
-                                                className={`
+                                            {/* BLOQUE DERECHO: La foto del index 6 (Ocupa las 2 columnas restantes y toda la altura) */}
+                                            {fotos[6] && (
+                                                <div
+                                                    onClick={() => setFotoSeleccionada(6)}
+                                                    className={`
                     col-span-1 relative overflow-hidden cursor-pointer rounded-2xl transition-all duration-200
                     ${fotoSeleccionada === 6 ? 'ring-4 ring-green scale-[0.98]' : 'hover:scale-[1.02]'}
                 `}
-                                            >
-                                                <img
-                                                    src={fotos[6]}
-                                                    alt="Foto 7"
-                                                    className="w-full h-full object-cover"
-                                                />
-                                            </div>
-                                        )}
+                                                >
+                                                    <img
+                                                        src={fotos[6]}
+                                                        alt="Foto 7"
+                                                        className="w-full h-full object-cover"
+                                                    />
+                                                </div>
+                                            )}
 
+                                        </div>
                                     </div>
-
                                 </div>
-                                <img src={LogoZeta} alt="Logo Zeta Channel" className="w-full h-auto mx-auto px-12" />
+
+                                <img src={LogoZeta} alt="Logo Zeta Channel" className="w-full h-auto mx-auto px-12 md:w-1/2 lg:px-32 lg:w-1/4 md:absolute md:bottom-30 md:left-50 lg:left-170 lg:right-1/2" />
+
                             </div>
                         </div>
                     )}
-                    {!popupHome && !popupQuepasa && (<div className={`absolute top-115 left-0 right-0 mx-auto pt-6 cursor-pointer flex flex-col lg:top-130 ${popupHome ? 'hidden' : 'block'}`} onClick={() => setpopupHome(true)}>
+                    {!popupHome && !popupQuepasa && (<div className={`absolute top-115 left-0 right-0 mx-auto pt-6 cursor-pointer flex flex-col md:top-120 lg:top-130 ${popupHome ? 'hidden' : 'block'}`} onClick={() => setpopupHome(true)}>
                         <div className={`relative w-30 h-20 mx-auto ${styles.animateheartbeat}`}>
                             <img src={pegatinaAlert} alt="Pegatina alerta" className="w-full h-auto mx-auto" />
                             <img src={IconoAlert} alt="Icono alerta" className="absolute bottom-0 right-0 left-0  mx-auto w-14 h-14" />
@@ -211,11 +215,17 @@ const Home = ({ popupHome, setpopupHome, popupQuepasa, setpopupQuepasa }) => {
                 <img src={IconoCarpeta} alt="Icono Carpeta" className="absolute -bottom-2 right-2 w-12 h-12" />
             </div>
 
-            <section className="mx-auto py-10 gap-16 px-5 flex flex-col">
-                <h2 className="text-4xl font-pixel text-black dark:text-white uppercase text-center">¿QUÉ TE APETECE RECORDAR?</h2>
-                <Card card={CATALOGO.find((peli) => peli.id === 1)} />
-                <Card card={CATALOGO.find((peli) => peli.id === 3)} />
-                <Card card={CATALOGO.find((peli) => peli.id === 18)} />
+            <section className="mx-auto py-10 gap-16 px-5 flex flex-col md:px-10">
+                <h2 className="text-4xl font-pixel text-black dark:text-white uppercase text-center md:text-5xl md:mb-5">¿QUÉ TE APETECE RECORDAR?</h2>
+                <div className="grid grid-cols-1 gap-16 md:gap-x-10 md:gap-y-18 md:grid-cols-2 lg:grid-cols-3 lg:container lg:mx-auto lg:px-30 lg:gap-20">
+                    <Card card={CATALOGO.find((peli) => peli.id === 1)} />
+                    <Card card={CATALOGO.find((peli) => peli.id === 3)} />
+                    <Card card={CATALOGO.find((peli) => peli.id === 18)} />
+                    <Card card={CATALOGO.find((peli) => peli.id === 14)} className="hidden md:block" />
+                    <Card card={CATALOGO.find((peli) => peli.id === 26)} className="hidden md:block" />
+                    <Card card={CATALOGO.find((peli) => peli.id === 34)} className="hidden md:block" />
+
+                </div>
                 <Btn to="/Cartelera" text="Ver cartelera" className="mx-auto" />
             </section>
 
@@ -224,16 +234,18 @@ const Home = ({ popupHome, setpopupHome, popupQuepasa, setpopupQuepasa }) => {
                 <Btn to="/Agenda" text="Ver Agenda" className="mx-auto py-6" />
             </section>
 
-            <div className="relative w-30 mb-25 py-12 h-20 mx-auto">
+            <div className="relative w-30 mb-25 py-12 h-20 mx-auto md:scale-110 md:mb-30">
                 <img src={pegatinaDinosauro} alt="Pegatina Dinosauro" className="mx-auto" />
                 <img src={IconoDinosauro} alt="Icono Dinosauro" className="absolute -bottom-14 left-2 w-14 h-14 dark:invert" />
             </div>
 
-            <section className="flex flex-col mx-6 gap-6 text-black dark:text-white">
-                <h2 className="text-4xl font-pixel uppercase text-center">¿Quienes somos?</h2>
-                <p>{informacion.parrafo1}</p>
-                <p>{informacion.parrafo2}</p>
-                <img src={informacion.imagen} alt="Imagen sobre nosotros" className="mx-auto my-6 bg-white overflow-hidden rounded-2xl" />
+            <section className="flex flex-col mx-6 gap-6 text-black dark:text-white md:gap-10 md:mx-8 lg:container lg:mx-auto lg:gap-12">
+                <h2 className="text-4xl font-pixel uppercase text-center md:text-5xl">¿Quienes somos?</h2>
+                <div className="md:flex md:gap-4 md:text-balance lg:ml-20">
+                    <p className="md:text-lg">{informacion.parrafo1}</p>
+                    <p className="md:text-lg">{informacion.parrafo2}</p>
+                </div>
+                <img src={informacion.imagen} alt="Imagen sobre nosotros" className="mx-auto my-6 bg-white overflow-hidden rounded-2xl md:w-3/4 md:my-1 lg:w-1/2" />
                 <Btn to="/Festival" text="Más Info" className="mx-auto mb-16" />
             </section>
         </>
